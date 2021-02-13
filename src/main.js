8a157722a8c9dd929f3a6f5ca4faacbc60b12dac
zhaojiuzhou <zhaojiuzhou@ymipro.com>
2021-02-13 09:32:57 +0800

    npm i vue-i18n -S
    多语言支持

--- src/main.js
+++ src/main.js
@@ -3,12 +3,14 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 import "./plugins/element.js";
+import i18n from "./tools/i18n";
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
+  i18n,
   render: function(h) {
     return h(App);
   }
